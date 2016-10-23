77b9e77d271c02d73bb27212b0a703ce0e517164
Sebastian Wicki <gandro@gmx.net>
2016-10-23 15:23:15 +0200

    Add working frontier examples

---
+++ examples/frontier_pub.rs
@@ -0,0 +1,36 @@
+extern crate timely;
+extern crate timely_query;
+
+use timely::dataflow::operators::*;
+use timely::dataflow::{Scope};
+use timely::progress::timestamp::RootTimestamp;
+use timely::dataflow::channels::pact::Pipeline;
+
+fn main() {
+    timely_query::execute(|root, coord| {
+        let (mut input, mut cap) = root.scoped(|scope| {
+            let (input, stream) = scope.new_unordered_input();
+
+            coord.publish_timely("frontier", &stream).unwrap();         
+
+            stream.unary_notify(Pipeline, "example", Vec::new(), |input, output, notificator| {
+                input.for_each(|time, data| {
+                    println!("pub frontier in input: {:?}", notificator.frontier(0));
+                    println!("pub {:?}: {:?}", &time.time(), &data[..]);
+                    output.session(&time).give_content(data);
+                });
+            });
+            input
+        });
+
+        let mut _default = cap.clone();
+        for round in 0..10i32 {
+            input.session(cap.clone()).give(round);
+            if round == 5 {
+                _default = cap.delayed(&RootTimestamp::new(10));
+            }
+            cap = cap.delayed(&RootTimestamp::new(round + 1));
+            root.step();
+        }
+    }).unwrap();
+}
---
+++ examples/frontier_sub.rs
@@ -0,0 +1,29 @@
+extern crate timely;
+extern crate timely_query;
+
+use timely::dataflow::operators::*;
+use timely::dataflow::{Scope};
+use timely::progress::timestamp::RootTimestamp;
+use timely::dataflow::channels::pact::Pipeline;
+
+fn main() {
+    timely_query::execute(|root, coord| {
+        let (mut input, mut cap) = root.scoped::<i32, _, _>(|scope| {
+            let (input, stream) = scope.new_unordered_input();
+            stream.unary_notify(Pipeline, "example", Vec::new(), |input, output, notificator| {
+                input.for_each(|time, data| {
+                    println!("sub frontier in input: {:?}", notificator.frontier(0));
+                    println!("sub {:?}: {:?}", &time.time(), &data[..]);
+                    output.session(&time).give_content(data);
+                });
+            });
+            input
+        });
+        
+        let sub = coord.timely_subscribe::<_, i32>("frontier".into(), true, cap).unwrap();
+        for (time, data) in sub {
+            input.session(time).give_iterator(data.into_iter());
+            root.step();
+        }
+    }).unwrap();
+}
