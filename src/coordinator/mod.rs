75feb7cae2b47b05a9be79e08693acfaa1b723cb
Sebastian Wicki <gandro@gmx.net>
2016-11-22 13:05:55 +0100

    Add cli flags for coordinator

--- src/coordinator/mod.rs
+++ src/coordinator/mod.rs
@@ -20,27 +20,55 @@ pub mod dispatch;
 
 mod util;
 
-pub fn coordinate(port: u16) -> Result<()> {
-    let network = Network::init(None)?; // TODO: topics must know external
-    let listener = network.listen(port)?;
-
-    let coordinate = futures::lazy(move || {
-        let catalog = Catalog::new(&network).expect("failed to create catalog"); // TODO
-        let coord = Coordinator::new(catalog);
-
-        listener.map(reqresp::multiplex).for_each(move |(tx, rx)| {
-            // every connection gets its own handle
-            let mut disp = Dispatch::new(coord.clone(), tx);
-            let client = rx.do_while(move |req| disp.dispatch(req))
-                .map_err(|err| {
-                    error!("failed to dispatch client: {:?}", err);
-                });
-
-            // handle client asynchronously
-            async::spawn(client);
-            Ok(())
-        })
-    });
-
-    async::finish(coordinate)
+pub struct Builder {
+    port: u16,
+    host: Option<String>,
 }
+
+impl Builder {
+    pub fn host(&mut self, host: String) {
+        self.host = Some(host);
+    }
+
+    pub fn port(&mut self, port: u16) {
+        self.port = port;
+    }
+}
+
+impl Default for Builder {
+    fn default() -> Self {
+        Builder {
+            host: None,
+            port: 9189,
+        }
+    }
+}
+
+impl Builder {
+    pub fn run(self) -> Result<()> {
+        let network = Network::init(self.host)?;
+        let listener = network.listen(self.port)?;
+
+        let coordinate = futures::lazy(move || {
+            let catalog = Catalog::new(&network).expect("failed to create catalog"); // TODO
+            let coord = Coordinator::new(catalog);
+
+            listener.map(reqresp::multiplex).for_each(move |(tx, rx)| {
+                // every connection gets its own handle
+                let mut disp = Dispatch::new(coord.clone(), tx);
+                let client = rx.do_while(move |req| disp.dispatch(req))
+                    .map_err(|err| {
+                        error!("failed to dispatch client: {:?}", err);
+                    });
+
+                // handle client asynchronously
+                async::spawn(client);
+                Ok(())
+            })
+        });
+
+        async::finish(coordinate)
+    }
+}
+
+
