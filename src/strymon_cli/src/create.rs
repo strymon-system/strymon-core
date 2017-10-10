7eef33231c927f6a3dfb6c7365e6d4cba66a2405
Sebastian Wicki <swicki@inf.ethz.ch>
2017-10-10 12:47:20 +0200

    Error handling skeleton in CLI

---
+++ src/strymon_cli/src/create.rs
@@ -0,0 +1,7 @@
+use clap::ArgMatches;
+
+use errors::*;
+
+pub fn run(_: &ArgMatches) -> Result<()> {
+    Err("not yet implemented")?
+}
--- src/strymon_cli/src/main.rs
+++ src/strymon_cli/src/main.rs
@@ -1,2 +1,37 @@
-fn main() {
+#[macro_use]
+extern crate error_chain;
+extern crate clap;
+
+mod errors;
+mod create;
+
+
+use clap::{App, AppSettings, Arg, SubCommand};
+
+use errors::*;
+
+quick_main!(dispatch);
+
+fn dispatch() -> Result<()> {
+    let matches = App::new("Strymon")
+        .version("0.1")
+        .author("Systems Group, ETH Zürich")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            SubCommand::with_name("create")
+                .about("Create a new Strymon application")
+                .arg(Arg::with_name("path").required(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("submit")
+                .about("Launch a Strymon application")
+                .arg(Arg::with_name("path").required(true)),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        ("create",  Some(matches)) => create::run(matches),
+        ("submit",   Some(matches)) => unimplemented!(),
+        _ => unreachable!("invalid subcommand"),
+    }
 }
