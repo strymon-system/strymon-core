060e2f5fdf8f7ae135bfafe750b82798dff1539e
Sebastian Wicki <swicki@inf.ethz.ch>
2017-10-10 11:21:12 +0200

    Remove unused facade library

--- lib/src/lib.rs
+++
@@ -1,5 +0,0 @@
-extern crate timely_system;
-
-// The query library really just is a facade for the system library.
-pub use timely_system::query::{Coordinator, execute, publish, subscribe};
-pub use timely_system::model;
