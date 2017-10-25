1e3630f89ac6728f10d2fc539af63bc9ee75d992
Sebastian Wicki <swicki@inf.ethz.ch>
2017-10-25 17:35:05 +0200

    Fix typo in inject-fault.sh

--- apps/topology-generator/inject-fault.sh
+++ apps/topology-generator/inject-fault.sh
@@ -2,7 +2,7 @@
 # this script is a simple bash tcp client for systems without telnet/netcat
 set -o errexit
 
-CMD=${1?"Usage: $0 <disconnect-random-switch|disconnect-random-switch> [HOSTNAME]"}
+CMD=${1?"Usage: $0 <disconnect-random-switch|disconnect-random-link> [HOSTNAME]"}
 HOST=${2:-localhost}
 PORT=9201
 
