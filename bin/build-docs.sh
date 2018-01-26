f4cf2b0a031fdcd3a2a27eb91e7b7385f913da1b
Sebastian Wicki <swicki@inf.ethz.ch>
2018-01-26 12:13:52 +0100

    Travis: Add deploy job for rustdoc
    Signed-off-by: Sebastian Wicki <swicki@inf.ethz.ch>

---
+++ bin/build-docs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eu
+BASEDIR=$(dirname "$0")
+INDEX="${BASEDIR}/../target/doc/index.html"
+
+CRATES="strymon_communication strymon_job strymon_model strymon_rpc"
+for crate in $CRATES ; do
+  cargo doc --no-deps --package $crate
+done
+
+echo "<meta http-equiv=refresh content=0;url=strymon_job/index.html>" > "${INDEX}"
