b5fa43c8d3182335a64b6881523ecf6cd7b33cde
Sebastian Wicki <swicki@inf.ethz.ch>
2018-02-20 10:09:00 +0100

    scripts: Check status when starting services
    Signed-off-by: Sebastian Wicki <swicki@inf.ethz.ch>

--- bin/start-strymon.sh
+++ bin/start-strymon.sh
@@ -4,6 +4,19 @@ set -eu -o pipefail
 BASEDIR=$(dirname "$0")
 . "${BASEDIR}/.common.sh"
 
+## Waits for a regex in the `strymon status` command to appear
+# $1: Full path to the the strymon binary
+# $2: Address of the coordinator
+# $3: Regex to match
+check_status() {
+  for i in $(seq 3); do
+    if "${1}" status -c "${2}" 2>&1 | grep -qE "${3}" ; then
+      return 0
+    fi
+    sleep 1
+  done
+}
+
 ## Starts a coordinator process
 # $1: Address of the coordinator to be spawned (e.g. localhost:9189)
 # $2: Working directory for pid and log files
@@ -20,12 +33,14 @@ start_coordinator() {
       ;;
   esac
 
+  coord_addr="${1}"
   coord_basedir="${2}"
   coord_binary="${3}"
 
   spawn_service "coordinator" "${coord_host}" "${coord_basedir}" \
     "${coord_binary}" --log-level "${RUST_LOG:-info}" manage start-coordinator \
     --external-hostname "${coord_host}" --port "${coord_port}"
+  check_status "${coord_binary}" "${coord_addr}" "^Coordinator:"
 }
 
 ## Starts an executor process
@@ -33,17 +48,20 @@ start_coordinator() {
 # $2: Working directory for pid and log files
 # $3: Full path to the the strymon binary
 # $4: Job working directory
+# $5: Address of the coordinator
 start_executor() {
   exec_host="$(echo ${1} | tr -d '[:space:]')"
   exec_basedir="${2}"
   exec_binary="${3}"
   exec_workdir="${4}"
+  exec_coord_addr="${5}"
 
   # TODO(swicki): This currently does not support the port range option
   spawn_service "executor" "${exec_host}" "${exec_basedir}" \
     "${coord_binary}" --log-level "${RUST_LOG:-info}" \
     manage start-executor --external-hostname "${exec_host}" \
-    --workdir "${exec_workdir}"
+    --workdir "${exec_workdir}" --coordinator "${exec_coord_addr}"
+  check_status "${exec_binary}" "${exec_coord_addr}" "Executor .* host=\"${exec_host}\""
 }
 
 #
@@ -62,6 +80,6 @@ FULL_BINARY="$(canonicalize_path "${BINARY}")"
 mkdir -p "${LOGDIR}"
 start_coordinator "${COORDINATOR}" "${LOGDIR}" "${FULL_BINARY}"
 while read host; do
-  start_executor "${host}" "${LOGDIR}" "${FULL_BINARY}" "${WORKDIR}"
+  start_executor "${host}" "${LOGDIR}" "${FULL_BINARY}" "${WORKDIR}" "${COORDINATOR}"
 done < "${EXECUTORS}"
 
